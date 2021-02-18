11c780a2e5776d939eb73f7e680242f3901e0f92
crow <caleb.farrell987@gmail.com>
2021-02-18 14:44:11 -0400

    README.md Created.

---
+++ README.md
@@ -0,0 +1,7 @@
+# CrowBot.py
+
+#Written By: Caleb 'Crow' Farrell
+
+#Date: 2020 - 10 - 15
+
+This bot will be enabled in my patio bar discord server to add some creativity and uniqueness.
---
+++ crow.py
@@ -0,0 +1,60 @@
+# Written By: The one, the only Crow
+# Date: Feb 18, 2021
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+SERVER = os.getenv('DISCORD_SERVER')
+
+intents = discord.Intents().all()
+client = discord.Client(intents = intents)
+
+def crowbot():
+    @client.event
+    async def on_ready():
+        for guild in client.guilds:
+            if guild.id == SERVER:
+                break
+        print(
+            f'{client.user} is connected to the following servers:\n'
+            f'{guild.name} (id: {guild.id})'
+        )
+        for member in guild.members:
+            print(member)
+        overlord = guild.get_member('Crow#2174')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        caw_message_intake = [
+            'caw',
+            'ca caw',
+            'caw!',
+            'look a crow!',
+            'it\'s a crow!',
+            'crow',
+            'crow#2174',
+            '@crow#2174',
+            '@crow'
+        ]
+        best_crow_message_intake = [
+            'im the best crow',
+            'i\'m the best crow',
+            '@Crow is the best crow'
+        ]
+        if message.author.id == 152993332526579712 and message.content.lower() == 'im the best':
+            response = "Behold! The almighty Crow!"
+            await message.channel.send(response)
+        if any(item in message.content.lower() for item in caw_message_intake):
+            response = 'CA CAW!!'
+            await message.channel.send(response)
+
+    client.run(TOKEN)
+
+if __name__ == '__main__':
+    crowbot()
+
+
