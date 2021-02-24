1b2142e83712518a77d9bd4328335e50f8c2e6d7
Caleb Farrell <caleb.farrell987@gmail.com>
2021-02-24 16:16:57 -0400

    Generated new file to switch from Client to bot. switched from .env to .json for simplicity. Renamed old python file to crowold.py and new one to crow.py.

--- crow.py
+++ crow.py
@@ -1,87 +1,52 @@
-# Written By: The one, the only Crow
-# Date: Feb 18, 2021
-import os
 import discord
-from dotenv import load_dotenv
-#TODO 2021-02-20 caleb:
+from discord.ext import commands
+import json
+
+#TODO 2021-02-24 caleb:
 # - convert from CLIENT to BOT
 # - change over basic text based commands to actual discord API commands
 # - create method to call for message - response interactions
 # - collect heartbeat data from MC server and display it's status with a command (probably something like !MCStatus)
+# - add Initial start flag for initial start flag
+# - transfer logic from old python script to new one
 
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-SERVER = os.getenv('DISCORD_SERVER')
-intents = discord.Intents().all()
-client = discord.Client(intents=intents)
+# Get configuration.json
+with open("configuration.json", "r") as config: 
+	data = json.load(config)
+	token = data["token"]
+	prefix = data["prefix"]
 
-#Roles
-bartender = '<@&794666590532665345>'
-admin = '<@&794628837891768340>'
-mod = '<@&794687361329266690>'
-def crowbot():
-    @client.event
-    async def on_ready():
-        for guild in client.guilds:
-            if guild.id == SERVER:
-                break
-        print(
-            f'{client.user} is connected to the following servers:\n'
-            f'{guild.name} (id: {guild.id})'
-        )
-        global members
-        members = '\n - '.join([member.name for member in guild.members])
+class Greetings(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		self._last_member = None
 
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
-        caw_message_intake = [
-            'caw',
-            'ca caw',
-            'caw!',
-            'look a crow!',
-            'it\'s a crow!',
-        ]
-        best_crow_message_intake = [
-            'im the best crow',
-            'i\'m the best crow',
-            '@Crow is the best crow'
-        ]
-        commands = [
-            '!members',
-            '!bartender',
-            '!admin',
-            '!mod',
-            '!commands'
-        ]
-        if message.author.id == 152993332526579712 and message.content.lower() == 'im the best':
-            response = "Behold! The almighty Crow!"
-            await message.channel.send(response)
-        if any(item in message.content.lower() for item in caw_message_intake):
-            response = 'CA CAW!!'
-            await message.channel.send(response)
-        if message.content.startswith('!members'):
-            response = members
-            await message.channel.send(response)
-        if message.content.startswith('!bartender'):
-            response = f'{bartender} you are being called!'
-            await message.channel.send(response)
-        if message.content.startswith('!admin'):
-            response = f'{admin} You are needed'
-            await message.channel.send(response)
-        if message.content.startswith('!mod'):
-            response =f'{mod} You are needed'
-            await message.channel.send(response)
-        if message.content.startswith('!commands'):
-            response = '\n'.join([item for item in commands])
-            #print( '\n'.join(item for item in commands))
-            await message.channel.send(response)
+# Intents
+intents = discord.Intents.default()
 
-    client.run(TOKEN)
+bot = commands.Bot(prefix, intents = intents)
+
+# Load cogs
+initial_extensions = [
+	"Cogs.onCommandError",
+	"Cogs.help",
+	"Cogs.ping"
+]
+
+print(initial_extensions)
 
 if __name__ == '__main__':
-    crowbot()
+	for extension in initial_extensions:
+		try:
+			bot.load_extension(extension)
+		except Exception as e:
+			print(f"Failed to load extension {extension}")
 
+@bot.event
+async def on_ready():
+	print(f"We have logged in as {bot.user}")
+	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name =f"{bot.command_prefix}help"))
+	print(discord.__version__)
 
+bot.run(token)
