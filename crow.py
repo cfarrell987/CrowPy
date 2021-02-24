0af9c5044834322ff9725e8a0fe99ca0561a8cd8
Caleb Farrell <caleb.farrell987@gmail.com>
2021-02-24 18:44:03 -0400

    Re-added on_message for replies. Added commands logic.

--- crow.py
+++ crow.py
@@ -10,12 +10,15 @@ import json
 # - add Initial start flag for initial start flag
 # - transfer logic from old python script to new one
 
+##INIT##
 
 # Get configuration.json
 with open("configuration.json", "r") as config: 
-	data = json.load(config)
-	token = data["token"]
-	prefix = data["prefix"]
+    data = json.load(config)
+    token = data["token"]
+    prefix = data["prefix"]
+    server = data["server"]
+
 
 class Greetings(commands.Cog):
 	def __init__(self, bot):
@@ -25,7 +28,7 @@ class Greetings(commands.Cog):
 # Intents
 intents = discord.Intents.default()
 
-bot = commands.Bot(prefix, intents = intents)
+bot = commands.Bot(command_prefix=prefix, intents = intents)
 
 # Load cogs
 initial_extensions = [
@@ -43,10 +46,79 @@ if __name__ == '__main__':
 		except Exception as e:
 			print(f"Failed to load extension {extension}")
 
+##RUNNING##
+
 @bot.event
 async def on_ready():
-	print(f"We have logged in as {bot.user}")
-	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name =f"{bot.command_prefix}help"))
-	print(discord.__version__)
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name =f"{bot.command_prefix}help"))
+    for guild in bot.guilds:
+        if guild.id == server:
+            break
+        print(
+            f'{bot.user} is connected to the following servers:\n'
+            f'{guild.name}'
+        )
+    global members
+    members = '\n - '.join([member.name for member in guild.members])
+    print(discord.__version__)
+
+
+
+@bot.command(name="members", aliases=["users","mem"])
+async def members(ctx):
+
+    await ctx.channel.send(members)
+
+
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+    if message.author == bot.user:
+        return
+    caw_message_intake = [
+            'caw',
+            'ca caw',
+            'caw!',
+            'look a crow!',
+            'it\'s a crow!',
+    ]
+    best_crow_message_intake = [
+            'im the best crow',
+            'i\'m the best crow',
+            '@Crow is the best crow'
+    ]
+    commands = [
+            '!members',
+            '!bartender',
+            '!admin',
+            '!mod',
+            '!commands'
+    ]
+    if message.author.id == 152993332526579712 and message.content.lower() == 'im the best':
+        response = "Behold! The almighty Crow!"
+        await message.channel.send(response)
+    if any(item in message.content.lower() for item in caw_message_intake):
+        response = 'CA CAW!!'
+        await message.channel.send(response)
+    if message.content.startswith('!members'):
+        response = members
+        await message.channel.send(response)
+    if message.content.startswith('!bartender'):
+        response = f'{bartender} you are being called!'
+        await message.channel.send(response)
+    if message.content.startswith('!admin'):
+        response = f'{admin} You are needed'
+        await message.channel.send(response)
+    if message.content.startswith('!mod'):
+        response =f'{mod} You are needed'
+        await message.channel.send(response)
+    if message.content.startswith('!commands'):
+        response = '\n'.join([item for item in commands])
+        #print( '\n'.join(item for item in commands))
+        await message.channel.send(response)
+
+
 
 bot.run(token)
+
+
